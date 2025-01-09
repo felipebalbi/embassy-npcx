0d6822eb2af9e75b1174ec1d32f7a1b1ef0ae347
David Venhoek <david@tweedegolf.com>
2025-01-09 11:28:33 +0100

    Implemented low-voltage input support.

--- src/gpio.rs
+++ src/gpio.rs
@@ -27,7 +27,7 @@ impl From<bool> for Level {
 }
 
 mod sealed {
-    pub trait SealedPin: Sized {
+    pub trait SealedPin {
         fn pin(&self) -> u8;
 
         fn port(&self) -> &'static crate::pac::gpio0::RegisterBlock;
@@ -37,6 +37,16 @@ mod sealed {
         /// caller must ensure it has a mutable reference to the pin or
         /// has otherwise elimnated contention.
         unsafe fn set_pin_function(&self, cs: critical_section::CriticalSection);
+
+        // Not ideal to mark this unsafe, but PeripheralRef is missing DerefMut...
+        /// Change whether the pin is low voltage, may do nothing on pins without
+        /// low voltage support.
+        /// Safety assumptions:
+        /// caller must ensure it has a mutable reference to the pin or
+        /// has otherwise elimnated contention.
+        /// caller must ensure this is not called to enable low voltage when the pin
+        /// is in a state where it can be damaged by doing that.
+        unsafe fn set_low_voltage(&self, cs: critical_section::CriticalSection, state: bool);
     }
 
     pub trait SealedInputPin {}
@@ -84,17 +94,23 @@ pub struct OutputOnly {}
 
 pub struct InputCapable {}
 
-pub struct Input<'d> {
+pub struct CanPullUp {}
+
+pub struct PullDownOnly {}
+
+pub struct Input<'d, T> {
     pin: PeripheralRef<'d, AnyPin>,
+    _phantom: PhantomData<T>,
 }
 
-impl<'d> Input<'d> {
+impl<'d> Input<'d, CanPullUp> {
     pub fn new(pin: impl Peripheral<P = impl InputPin + 'd> + 'd) -> Self {
         into_ref!(pin);
 
         critical_section::with(|cs| {
             // Safety:
             // We have a mutable reference to the pin through PeripheralRef
+            unsafe { pin.set_low_voltage(cs, false) };
             unsafe { pin.set_pin_function(cs) };
 
             let regs = pin.port();
@@ -102,15 +118,10 @@ impl<'d> Input<'d> {
             regs.px_dir().modify(|_, w| w.pin(pin.pin()).input());
         });
 
-        Input { pin: pin.map_into() }
-    }
-
-    pub fn disable_pull(&mut self) {
-        critical_section::with(|_| {
-            let regs = self.pin.port();
-
-            regs.px_pull().modify(|_, w| w.pin(self.pin.pin()).disabled());
-        });
+        Input {
+            pin: pin.map_into(),
+            _phantom: PhantomData,
+        }
     }
 
     pub fn enable_pullup(&mut self) {
@@ -119,6 +130,50 @@ impl<'d> Input<'d> {
 
             regs.px_pud().modify(|_, w| w.pin(self.pin.pin()).pull_up());
             regs.px_pull().modify(|_, w| w.pin(self.pin.pin()).enabled());
+        });
+    }
+
+    #[must_use]
+    pub fn degrade(self) -> Input<'d, PullDownOnly> {
+        Input {
+            pin: self.pin,
+            _phantom: PhantomData,
+        }
+    }
+}
+
+impl<'d> Input<'d, PullDownOnly> {
+    pub fn new_lowvoltage(pin: impl Peripheral<P = impl LowVoltagePin + 'd> + 'd) -> Self {
+        into_ref!(pin);
+
+        critical_section::with(|cs| {
+            let regs = pin.port();
+
+            regs.px_dir().modify(|_, w| w.pin(pin.pin()).input());
+            regs.px_pud().modify(|_, w| w.pin(pin.pin()).pull_down());
+            // Required for low voltage operations
+            regs.px_otype().modify(|_, w| w.pin(pin.pin()).opendrain());
+
+            // Safety:
+            // We have a mutable reference to the pin through PeripheralRef
+            // Everything is put in a safe state for low voltage above
+            unsafe { pin.set_pin_function(cs) };
+            unsafe { pin.set_low_voltage(cs, true) };
+        });
+
+        Self {
+            pin: pin.map_into(),
+            _phantom: PhantomData,
+        }
+    }
+}
+
+impl<T> Input<'_, T> {
+    pub fn disable_pull(&mut self) {
+        critical_section::with(|_| {
+            let regs = self.pin.port();
+
+            regs.px_pull().modify(|_, w| w.pin(self.pin.pin()).disabled());
         });
     }
 
@@ -131,6 +186,7 @@ impl<'d> Input<'d> {
         });
     }
 
+    #[must_use]
     pub fn is_low(&self) -> bool {
         critical_section::with(|_| {
             let regs = self.pin.port();
@@ -139,6 +195,7 @@ impl<'d> Input<'d> {
         })
     }
 
+    #[must_use]
     pub fn is_high(&self) -> bool {
         critical_section::with(|_| {
             let regs = self.pin.port();
@@ -160,6 +217,7 @@ impl<'d> OutputOpenDrain<'d, OutputOnly> {
         critical_section::with(|cs| {
             // Safety:
             // We have a mutable reference to the pin through PeripheralRef
+            unsafe { pin.set_low_voltage(cs, false) };
             unsafe { pin.set_pin_function(cs) };
 
             let regs = pin.port();
@@ -184,6 +242,7 @@ impl<'d> OutputOpenDrain<'d, InputCapable> {
         critical_section::with(|cs| {
             // Safety:
             // We have a mutable reference to the pin through PeripheralRef
+            unsafe { pin.set_low_voltage(cs, false) };
             unsafe { pin.set_pin_function(cs) };
 
             let regs = pin.port();
@@ -265,6 +324,7 @@ impl<T> OutputOpenDrain<'_, T> {
 }
 
 impl OutputOpenDrain<'_, InputCapable> {
+    #[must_use]
     pub fn is_low(&self) -> bool {
         critical_section::with(|_| {
             let regs = self.pin.port();
@@ -273,6 +333,7 @@ impl OutputOpenDrain<'_, InputCapable> {
         })
     }
 
+    #[must_use]
     pub fn is_high(&self) -> bool {
         critical_section::with(|_| {
             let regs = self.pin.port();
@@ -280,19 +341,35 @@ impl OutputOpenDrain<'_, InputCapable> {
             regs.px_din().read().pin(self.pin.pin()).is_high()
         })
     }
+
+    pub fn drive_always(&self) {
+        critical_section::with(|_| {
+            let regs = self.pin.port();
+            regs.px_envdd().modify(|_, w| w.pin(self.pin.pin()).disabled());
+        })
+    }
+
+    pub fn drive_when_vdd_present(&self) {
+        critical_section::with(|_| {
+            let regs = self.pin.port();
+            regs.px_envdd().modify(|_, w| w.pin(self.pin.pin()).enabled());
+        })
+    }
 }
 
-pub struct Output<'d> {
+pub struct Output<'d, T> {
     pin: PeripheralRef<'d, AnyPin>,
+    _phantom: PhantomData<T>,
 }
 
-impl<'d> Output<'d> {
+impl<'d> Output<'d, OutputOnly> {
     pub fn new(pin: impl Peripheral<P = impl Pin + 'd> + 'd, level: Level) -> Self {
         into_ref!(pin);
 
         critical_section::with(|cs| {
             // Safety:
             // We have a mutable reference to the pin through PeripheralRef
+            unsafe { pin.set_low_voltage(cs, false) };
             unsafe { pin.set_pin_function(cs) };
 
             let regs = pin.port();
@@ -303,9 +380,72 @@ impl<'d> Output<'d> {
             regs.px_dir().modify(|_, w| w.pin(pin.pin()).output());
         });
 
-        Output { pin: pin.map_into() }
+        Output {
+            pin: pin.map_into(),
+            _phantom: PhantomData,
+        }
+    }
+}
+
+impl<'d> Output<'d, InputCapable> {
+    pub fn new(pin: impl Peripheral<P = impl InputPin + 'd> + 'd, level: Level) -> Self {
+        into_ref!(pin);
+
+        critical_section::with(|cs| {
+            // Safety:
+            // We have a mutable reference to the pin through PeripheralRef
+            unsafe { pin.set_low_voltage(cs, false) };
+            unsafe { pin.set_pin_function(cs) };
+
+            let regs = pin.port();
+
+            // Set data and output direction
+            regs.px_dout().modify(|_, w| w.pin(pin.pin()).bit(level.into()));
+            regs.px_otype().modify(|_, w| w.pin(pin.pin()).pushpull());
+            regs.px_dir().modify(|_, w| w.pin(pin.pin()).output());
+        });
+
+        Output {
+            pin: pin.map_into(),
+            _phantom: PhantomData,
+        }
     }
 
+    pub fn new_lowvoltage(pin: impl Peripheral<P = impl LowVoltagePin + 'd> + 'd, level: Level) -> Self {
+        into_ref!(pin);
+
+        critical_section::with(|cs| {
+            let regs = pin.port();
+
+            // Required for low voltage operation
+            regs.px_pud().modify(|_, w| w.pin(pin.pin()).pull_down());
+            regs.px_otype().modify(|_, w| w.pin(pin.pin()).opendrain());
+            regs.px_dir().modify(|_, w| w.pin(pin.pin()).output());
+            regs.px_dout().modify(|_, w| w.pin(pin.pin()).bit(level.into()));
+
+            // Safety:
+            // We have a mutable reference to the pin through PeripheralRef
+            // Everything is put in a safe state for low voltage above
+            unsafe { pin.set_pin_function(cs) };
+            unsafe { pin.set_low_voltage(cs, true) };
+        });
+
+        Self {
+            pin: pin.map_into(),
+            _phantom: PhantomData,
+        }
+    }
+
+    #[must_use]
+    pub fn degrade(self) -> Output<'d, OutputOnly> {
+        Output {
+            pin: self.pin,
+            _phantom: PhantomData,
+        }
+    }
+}
+
+impl<T> Output<'_, T> {
     pub fn set_low(&mut self) {
         critical_section::with(|_| {
             let regs = self.pin.port();
@@ -361,6 +501,22 @@ impl<'d> Output<'d> {
     }
 }
 
+impl Output<'_, InputCapable> {
+    pub fn drive_always(&self) {
+        critical_section::with(|_| {
+            let regs = self.pin.port();
+            regs.px_envdd().modify(|_, w| w.pin(self.pin.pin()).disabled());
+        })
+    }
+
+    pub fn drive_when_vdd_present(&self) {
+        critical_section::with(|_| {
+            let regs = self.pin.port();
+            regs.px_envdd().modify(|_, w| w.pin(self.pin.pin()).enabled());
+        })
+    }
+}
+
 pub trait LowVoltagePin: InputPin + sealed::SealedLowVoltagePin {}
 
 pub trait InputPin: Pin + sealed::SealedInputPin {}
@@ -369,6 +525,9 @@ pub trait Pin: sealed::SealedPin {}
 
 macro_rules! impl_pin {
     ($peripheral:ident, $port:expr, $pin:expr, $pin_function:expr) => {
+        impl_pin!($peripheral, $port, $pin, $pin_function, |_, _| {});
+    };
+    ($peripheral:ident, $port:expr, $pin:expr, $pin_function:expr, $low_voltage_function:expr) => {
         impl sealed::SealedPin for crate::peripherals::$peripheral {
             fn pin(&self) -> u8 {
                 $pin
@@ -386,11 +545,19 @@ macro_rules! impl_pin {
             unsafe fn set_pin_function(&self, _cs: critical_section::CriticalSection) {
                 // Safety:
                 // We have the unique reference to the pin and are within a critical section, so no contention
-                // Note: allow unused as not every pin is dual function
                 fn internal_set(f: impl FnOnce(crate::pac::Sysconfig)) {
                     f(unsafe { crate::pac::Sysconfig::steal() });
                 }
                 internal_set($pin_function);
+            }
+
+            unsafe fn set_low_voltage(&self, _cs: critical_section::CriticalSection, state: bool) {
+                // Safety:
+                // We have the unique reference to the pin and are within a critical section, so no contention
+                fn internal_set(f: impl FnOnce(crate::pac::Sysconfig, bool), state: bool) {
+                    f(unsafe { crate::pac::Sysconfig::steal() }, state);
+                }
+                internal_set($low_voltage_function, state);
             }
         }
         impl Pin for crate::peripherals::$peripheral {}
@@ -399,15 +566,18 @@ macro_rules! impl_pin {
 
 macro_rules! impl_input_pin {
     ($peripheral:ident, $port:expr, $pin:expr, $pin_function:expr) => {
-        impl_pin!($peripheral, $port, $pin, $pin_function);
+        impl_input_pin!($peripheral, $port, $pin, $pin_function, |_, _| {});
+    };
+    ($peripheral:ident, $port:expr, $pin:expr, $pin_function:expr, $low_voltage_function:expr) => {
+        impl_pin!($peripheral, $port, $pin, $pin_function, $low_voltage_function);
         impl sealed::SealedInputPin for crate::peripherals::$peripheral {}
         impl InputPin for crate::peripherals::$peripheral {}
     };
 }
 
 macro_rules! impl_lowvoltage_pin {
-    ($peripheral:ident, $port:expr, $pin:expr, $pin_function:expr) => {
-        impl_input_pin!($peripheral, $port, $pin, $pin_function);
+    ($peripheral:ident, $port:expr, $pin:expr, $pin_function:expr, $low_voltage_function:expr) => {
+        impl_input_pin!($peripheral, $port, $pin, $pin_function, $low_voltage_function);
         impl sealed::SealedLowVoltagePin for crate::peripherals::$peripheral {}
         impl LowVoltagePin for crate::peripherals::$peripheral {}
     };
@@ -415,28 +585,87 @@ macro_rules! impl_lowvoltage_pin {
 
 // GPIOB
 impl_input_pin!(PL11, crate::pac::Gpiob::ptr(), 0, |_| {});
-impl_lowvoltage_pin!(PD08, crate::pac::Gpiob::ptr(), 1, |regs| {
-    regs.devalta().modify(|_, w| w.no_kso17_sl().set_bit());
-    regs.devalte().modify(|_, w| w.cr_sin4_sl().clear_bit());
-});
-impl_lowvoltage_pin!(PK10, crate::pac::Gpiob::ptr(), 2, |regs| {
-    regs.devalt2().modify(|_, w| w.i2c7_0_sl().clear_bit());
-    regs.devaltb().modify(|_, w| w.dsr_sl().clear_bit());
-});
-impl_lowvoltage_pin!(PJ09, crate::pac::Gpiob::ptr(), 3, |regs| {
-    regs.devalt2().modify(|_, w| w.i2c7_0_sl().clear_bit());
-    regs.devaltb().modify(|_, w| w.dcd_sl().clear_bit());
-});
-impl_lowvoltage_pin!(PB12, crate::pac::Gpiob::ptr(), 4, |regs| {
-    regs.devalt2().modify(|_, w| w.i2c0_0_sl().clear_bit());
-});
-impl_lowvoltage_pin!(PC12, crate::pac::Gpiob::ptr(), 5, |regs| {
-    regs.devalt2().modify(|_, w| w.i2c0_0_sl().clear_bit());
-});
-impl_pin!(PL09, crate::pac::Gpiob::ptr(), 6, |regs| {
-    regs.devalt4().modify(|_, w| w.pwm4_led0_sl().clear_bit());
-});
-impl_lowvoltage_pin!(PJ07, crate::pac::Gpiob::ptr(), 7, |regs| {
-    regs.devalt4().modify(|_, w| w.pwm4_led0_sl().clear_bit());
-    regs.devaltk().modify(|_, w| w.i2c7_1_sl().clear_bit());
-});
+impl_lowvoltage_pin!(
+    PD08,
+    crate::pac::Gpiob::ptr(),
+    1,
+    |regs| {
+        regs.devalta().modify(|_, w| w.no_kso17_sl().set_bit());
+        regs.devalte().modify(|_, w| w.cr_sin4_sl().clear_bit());
+    },
+    |regs, state| {
+        regs.lv_gpio_ctla().modify(|_, w| w.gb1_lv().bit(state));
+    }
+);
+impl_lowvoltage_pin!(
+    PK10,
+    crate::pac::Gpiob::ptr(),
+    2,
+    |regs| {
+        regs.devalt2().modify(|_, w| w.i2c7_0_sl().clear_bit());
+        regs.devaltb().modify(|_, w| w.dsr_sl().clear_bit());
+    },
+    |regs, state| {
+        regs.lv_gpio_ctl0().modify(|_, w| w.gb2_lv().bit(state));
+    }
+);
+impl_lowvoltage_pin!(
+    PJ09,
+    crate::pac::Gpiob::ptr(),
+    3,
+    |regs| {
+        regs.devalt2().modify(|_, w| w.i2c7_0_sl().clear_bit());
+        regs.devaltb().modify(|_, w| w.dcd_sl().clear_bit());
+    },
+    |regs, state| {
+        regs.lv_gpio_ctl0().modify(|_, w| w.gb3_lv().bit(state));
+    }
+);
+impl_lowvoltage_pin!(
+    PB12,
+    crate::pac::Gpiob::ptr(),
+    4,
+    |regs| {
+        regs.devalt2().modify(|_, w| w.i2c0_0_sl().clear_bit());
+    },
+    |regs, state| {
+        regs.lv_gpio_ctl0().modify(|_, w| w.gb4_lv().bit(state));
+    }
+);
+impl_lowvoltage_pin!(
+    PC12,
+    crate::pac::Gpiob::ptr(),
+    5,
+    |regs| {
+        regs.devalt2().modify(|_, w| w.i2c0_0_sl().clear_bit());
+    },
+    |regs, state| {
+        regs.lv_gpio_ctl0().modify(|_, w| w.gb5_lv().bit(state));
+    }
+);
+impl_pin!(
+    PL09,
+    crate::pac::Gpiob::ptr(),
+    6,
+    |regs| {
+        regs.devalt4().modify(|_, w| w.pwm4_led0_sl().clear_bit());
+    },
+    |regs, state| {
+        // Despite the datasheet stating this is a output only port
+        // it still documents a low-voltage input register for it. We
+        // implement it here for safety
+        regs.lv_gpio_ctla().modify(|_, w| w.gb6_lv().bit(state));
+    }
+);
+impl_lowvoltage_pin!(
+    PJ07,
+    crate::pac::Gpiob::ptr(),
+    7,
+    |regs| {
+        regs.devalt4().modify(|_, w| w.pwm4_led0_sl().clear_bit());
+        regs.devaltk().modify(|_, w| w.i2c7_1_sl().clear_bit());
+    },
+    |regs, state| {
+        regs.lv_gpio_ctla().modify(|_, w| w.gb7_lv().bit(state));
+    }
+);
