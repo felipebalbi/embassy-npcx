178b093d1116661b6849efc417998c9e49e0b37c
Wouter Geraedts <git@woutergeraedts.nl>
2025-01-16 10:06:48 +0100

    Cleanup up MIWU API and use macro's to define channels

--- src/miwu.rs
+++ src/miwu.rs
@@ -134,8 +134,8 @@ pub trait WakeUpInput: sealed::SealedWakeUpInput {
 }
 
 macro_rules! impl_wake_up_input {
-    ($peripheral:ident, $miwu_n:literal, $group:literal, $subgroup:literal, $interrupt:ident) => {
-        impl sealed::SealedWakeUpInput for crate::peripherals::$peripheral {
+    ($peripheral:ty, $miwu_n:expr, $group:expr, $subgroup:expr, $interrupt:ident) => {
+        impl sealed::SealedWakeUpInput for $peripheral {
             fn group(&self) -> usize {
                 $group
             }
@@ -149,10 +149,10 @@ macro_rules! impl_wake_up_input {
                 crate::pac::Interrupt::$interrupt
             }
         }
-        impl WakeUpInput for crate::peripherals::$peripheral {}
+        impl WakeUpInput for $peripheral {}
 
         #[cfg(feature = "rt")]
-        impl rt::WakeUpInputWaitable for crate::peripherals::$peripheral {}
+        impl rt::WakeUpInputWaitable for $peripheral {}
     };
 }
 
@@ -182,18 +182,28 @@ pub mod rt {
     }
 
     pub trait WakeUpInputWaitable: WakeUpInput + Sized {
-        async fn wait_for_pending(&mut self, mode: Mode) {
+        fn wait_for(&mut self, mode: Mode) -> impl Future {
             self.enable(mode);
-            WakeUpInputFuture::<Self> { channel: self }.await
+            WakeUpInputFuture::<Self> { channel: self }
         }
 
-        fn waker(&self) -> &'static AtomicWaker {
-            get_wui(self.port_n(), self.group(), self.subgroup())
+        fn wait_for_high(&mut self) -> impl Future {
+            self.wait_for(Mode::Level(Level::High))
+        }
+
+        fn wait_for_low(&mut self) -> impl Future {
+            self.wait_for(Mode::Level(Level::Low))
         }
     }
 
     struct WakeUpInputFuture<'a, T: WakeUpInputWaitable> {
         channel: &'a mut T,
+    }
+
+    impl<'a, T: WakeUpInputWaitable> WakeUpInputFuture<'a, T> {
+        fn waker(&self) -> &'static AtomicWaker {
+            get_wui(self.channel.port_n(), self.channel.group(), self.channel.subgroup())
+        }
     }
 
     impl<'a, T: WakeUpInputWaitable> Drop for WakeUpInputFuture<'a, T> {
@@ -208,7 +218,7 @@ pub mod rt {
         type Output = ();
 
         fn poll(self: core::pin::Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-            self.channel.waker().register(cx.waker());
+            self.waker().register(cx.waker());
 
             if self.channel.is_pending() {
                 Poll::Ready(())
@@ -234,55 +244,106 @@ pub mod rt {
         }
     }
 
-    fn on_irq(miwu_n: usize, miwu_group: usize) {
+    fn on_irq(miwu_n: usize, group: usize) {
         let port = get_miwu(miwu_n);
 
-        let pending = port.wkpndn(miwu_group).read();
+        let pending = port.wkpndn(group).read();
         for subgroup in BitIter(pending.bits()) {
-            let waker = get_wui(miwu_n, miwu_group, subgroup as usize);
+            let waker = get_wui(miwu_n, group, subgroup as usize);
             waker.wake();
         }
 
         critical_section::with(|_cs| {
-            port.wkenn(miwu_group)
+            port.wkenn(group)
                 .modify(|r, w| unsafe { w.bits(r.bits() & !pending.bits()) });
         });
     }
 
     macro_rules! impl_irq {
-        ($interrupt:ident, $miwu_n:literal, $miwu_group:literal) => {
+        ($interrupt:ident, $miwu_n:literal, $group:literal) => {
             #[allow(non_snake_case)]
             #[interrupt]
             unsafe fn $interrupt() {
-                on_irq($miwu_n, $miwu_group)
+                on_irq($miwu_n, $group - 1) // The groups are 1-indexed
             }
         };
     }
 
-    impl_irq!(WKINTA_0, 0, 0);
-    impl_irq!(WKINTB_0, 0, 1);
-    impl_irq!(WKINTC_0, 0, 2);
-    impl_irq!(WKINTD_0, 0, 3);
-    impl_irq!(WKINTE_0, 0, 4);
-    impl_irq!(WKINTF_0, 0, 5);
-    impl_irq!(WKINTG_0, 0, 6);
-    impl_irq!(WKINTH_0, 0, 7);
-    impl_irq!(WKINTA_1, 1, 0);
-    impl_irq!(WKINTB_1, 1, 1);
-    impl_irq!(WKINTC_1, 1, 2);
-    impl_irq!(WKINTD_1, 1, 3);
-    impl_irq!(WKINTE_1, 1, 4);
-    impl_irq!(WKINTF_1, 1, 5);
-    impl_irq!(WKINTG_1, 1, 6);
-    impl_irq!(WKINTH_1, 1, 7);
-    impl_irq!(WKINTA_2, 2, 0);
-    impl_irq!(WKINTB_2, 2, 1);
-    impl_irq!(WKINTC_2, 2, 2);
-    impl_irq!(WKINTD_2, 2, 3);
-    impl_irq!(WKINTE_2, 2, 4);
-    impl_irq!(WKINTF_2, 2, 5);
-    impl_irq!(WKINTG_2, 2, 6);
-    impl_irq!(WKINTH_2, 2, 7);
+    impl_irq!(WKINTA_0, 0, 1);
+    impl_irq!(WKINTB_0, 0, 2);
+    impl_irq!(WKINTC_0, 0, 3);
+    impl_irq!(WKINTD_0, 0, 4);
+    impl_irq!(WKINTE_0, 0, 5);
+    impl_irq!(WKINTF_0, 0, 6);
+    impl_irq!(WKINTG_0, 0, 7);
+    impl_irq!(WKINTH_0, 0, 8);
+    impl_irq!(WKINTA_1, 1, 1);
+    impl_irq!(WKINTB_1, 1, 2);
+    impl_irq!(WKINTC_1, 1, 3);
+    impl_irq!(WKINTD_1, 1, 4);
+    impl_irq!(WKINTE_1, 1, 5);
+    impl_irq!(WKINTF_1, 1, 6);
+    impl_irq!(WKINTG_1, 1, 7);
+    impl_irq!(WKINTH_1, 1, 8);
+    impl_irq!(WKINTA_2, 2, 1);
+    impl_irq!(WKINTB_2, 2, 2);
+    impl_irq!(WKINTC_2, 2, 3);
+    impl_irq!(WKINTD_2, 2, 4);
+    impl_irq!(WKINTE_2, 2, 5);
+    impl_irq!(WKINTF_2, 2, 6);
+    impl_irq!(WKINTG_2, 2, 7);
+    impl_irq!(WKINTH_2, 2, 8);
 }
 
-impl_wake_up_input!(MIWU1_73, 1, 6, 3, WKINTG_1);
+use paste::paste;
+
+macro_rules! impl_wake_up_input_n {
+    ($miwu_n:literal, $group:literal, $subgroup:literal, $interrupt:ident) => {
+        paste! { impl_wake_up_input!(
+                crate::peripherals::[<MIWU $miwu_n _ $group $subgroup>],
+                $miwu_n,
+                ($group - 1), // The groups are 1-indexed
+                $subgroup,
+                $interrupt
+            );
+        }
+    };
+}
+
+macro_rules! impl_wake_up_input_nm {
+    ($miwu_n:literal, $group:literal, $interrupt:ident) => {
+        impl_wake_up_input_n!($miwu_n, $group, 0, $interrupt);
+        impl_wake_up_input_n!($miwu_n, $group, 1, $interrupt);
+        impl_wake_up_input_n!($miwu_n, $group, 2, $interrupt);
+        impl_wake_up_input_n!($miwu_n, $group, 3, $interrupt);
+        impl_wake_up_input_n!($miwu_n, $group, 4, $interrupt);
+        impl_wake_up_input_n!($miwu_n, $group, 5, $interrupt);
+        impl_wake_up_input_n!($miwu_n, $group, 6, $interrupt);
+        impl_wake_up_input_n!($miwu_n, $group, 7, $interrupt);
+    };
+}
+
+impl_wake_up_input_nm!(0, 1, WKINTA_0);
+impl_wake_up_input_nm!(0, 2, WKINTB_0);
+impl_wake_up_input_nm!(0, 3, WKINTC_0);
+impl_wake_up_input_nm!(0, 4, WKINTD_0);
+impl_wake_up_input_nm!(0, 5, WKINTE_0);
+impl_wake_up_input_nm!(0, 6, WKINTF_0);
+impl_wake_up_input_nm!(0, 7, WKINTG_0);
+impl_wake_up_input_nm!(0, 8, WKINTH_0);
+impl_wake_up_input_nm!(1, 1, WKINTA_1);
+impl_wake_up_input_nm!(1, 2, WKINTB_1);
+impl_wake_up_input_nm!(1, 3, WKINTC_1);
+impl_wake_up_input_nm!(1, 4, WKINTD_1);
+impl_wake_up_input_nm!(1, 5, WKINTE_1);
+impl_wake_up_input_nm!(1, 6, WKINTF_1);
+impl_wake_up_input_nm!(1, 7, WKINTG_1);
+impl_wake_up_input_nm!(1, 8, WKINTH_1);
+impl_wake_up_input_nm!(2, 1, WKINTA_2);
+impl_wake_up_input_nm!(2, 2, WKINTB_2);
+impl_wake_up_input_nm!(2, 3, WKINTC_2);
+impl_wake_up_input_nm!(2, 4, WKINTD_2);
+impl_wake_up_input_nm!(2, 5, WKINTE_2);
+impl_wake_up_input_nm!(2, 6, WKINTF_2);
+impl_wake_up_input_nm!(2, 7, WKINTG_2);
+impl_wake_up_input_nm!(2, 8, WKINTH_2);
