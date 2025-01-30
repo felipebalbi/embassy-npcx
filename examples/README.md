cd5074b1655d378f940d73de389a75f3f21f3258
Dion Dokter <dion@tweedegolf.com>
2025-01-30 11:24:40 +0100

    Move HAL examples to inside the repo

---
+++ examples/README.md
@@ -0,0 +1,13 @@
+# NPCX490M Examples
+
+A collection of examples demonstrating the use of the
+[npcx490m-pac](https://github.com/pop-project/npcx490m-pac) crate.
+
+## Dependencies
+
+This project relies on [flip-link](https://crates.io/crates/flip-link)
+for linking. Make sure to install it with:
+
+```console
+$ cargo install flip-link
+```
---
+++ examples/src/bin/miwu_gpio.rs
@@ -0,0 +1,34 @@
+#![no_main]
+#![no_std]
+
+use defmt::info;
+use embassy_executor::Spawner;
+use embassy_npcx::gpio::{Level, Output, OutputOnly};
+use embassy_npcx::gpio_miwu::AwaitableInput;
+use embassy_npcx::interrupt::InterruptExt;
+use embassy_npcx::{self as hal, Config};
+use embedded_hal_async::digital::Wait;
+use {defmt_rtt as _, panic_probe as _};
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    info!("To use, configure JP2 to '2' and press SW1. LED D8 should light up.");
+
+    let p = embassy_npcx::init(Config::default());
+
+    let mut button = AwaitableInput::new(p.PJ02, p.MIWU1_73);
+    button.enable_pullup();
+
+    let mut led = Output::<'_, OutputOnly>::new(p.PJ07, Level::High);
+    unsafe { hal::interrupt::WKINTG_1.enable() };
+
+    loop {
+        button.wait_for_low().await.unwrap();
+        led.set_low();
+        info!("Low");
+
+        button.wait_for_high().await.unwrap();
+        led.set_high();
+        info!("High");
+    }
+}
